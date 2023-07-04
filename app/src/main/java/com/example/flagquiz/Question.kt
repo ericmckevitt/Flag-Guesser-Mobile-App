16ca67996e698a1fb464e9646a0b1361b648176f
Eric McKevitt <erictmckevitt@gmail.com>
2023-07-04 17:52:30 -0500

    Added questions in constants object

--- app/src/main/java/com/example/flagquiz/Question.kt
+++ app/src/main/java/com/example/flagquiz/Question.kt
@@ -2,7 +2,7 @@ package com.example.flagquiz
 
 data class Question(
     val id: Int,
-    val questions: String,
+    val question: String,
     val image: Int,
     val optionOne: String,
     val optionTwo: String,
